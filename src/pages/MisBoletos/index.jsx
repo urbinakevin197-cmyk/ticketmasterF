1a999c6598e40bc0b984e3a19616a0ada0cc860a
urbinakevin197-cmyk <urbinakevin197@gmail.com>
2026-04-08 21:43:30 -0600

    elvia

--- src/pages/MisBoletos/index.jsx
+++ src/pages/MisBoletos/index.jsx
@@ -640,7 +640,7 @@ useEffect(() => {
                     ¡Bienvenido de vuelta!
                   </span>
                   <span className="sc-8486e8e9-3 iTJOrD" data-cs-mask="true">
-                    Dagne Yedith
+                    Elvia España
                   </span>
                 </p>
               </div>
--- src/pages/Navbar/index.jsx
+++ src/pages/Navbar/index.jsx
@@ -49,7 +49,7 @@ useEffect(() => {
         {/* BIENVENIDA */}
         <div className="welcome">
           <span>¡Bienvenido de vuelta!</span>
-          <strong>Dagne Yedith</strong>
+          <strong>Elvia España</strong>
         </div>
 
         {/* MIS BOLETOS */}
