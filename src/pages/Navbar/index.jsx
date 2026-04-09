e0fea1b03b6f7caa2dcefb9671b751f5ac3e9471
urbinakevin197-cmyk <urbinakevin197@gmail.com>
2026-04-08 20:42:14 -0600

    dagne

--- src/pages/Navbar/index.jsx
+++ src/pages/Navbar/index.jsx
@@ -49,7 +49,7 @@ useEffect(() => {
         {/* BIENVENIDA */}
         <div className="welcome">
           <span>¡Bienvenido de vuelta!</span>
-          <strong>ALondra Guadalupe</strong>
+          <strong>Dagne Yedith</strong>
         </div>
 
         {/* MIS BOLETOS */}
