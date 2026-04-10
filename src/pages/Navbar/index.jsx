0577b63a33d9a9d563c38af9a33aec2d15ff1925
urbinakevin197-cmyk <urbinakevin197@gmail.com>
2026-04-10 15:02:00 -0600

    isabel

--- src/pages/Navbar/index.jsx
+++ src/pages/Navbar/index.jsx
@@ -49,7 +49,7 @@ useEffect(() => {
         {/* BIENVENIDA */}
         <div className="welcome">
           <span>¡Bienvenido de vuelta!</span>
-          <strong>Elvia España</strong>
+          <strong>Isabel Pozos</strong>
         </div>
 
         {/* MIS BOLETOS */}
