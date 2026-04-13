a9c2d1156c51987397ebe2a3d708ed51d30278d2
urbinakevin197-cmyk <urbinakevin197@gmail.com>
2026-04-13 13:04:48 -0600

    alexa barojas

--- src/pages/Navbar/index.jsx
+++ src/pages/Navbar/index.jsx
@@ -49,7 +49,7 @@ useEffect(() => {
         {/* BIENVENIDA */}
         <div className="welcome">
           <span>¡Bienvenido de vuelta!</span>
-          <strong>Isabel Pozos</strong>
+          <strong>Alexa Barojas</strong>
         </div>
 
         {/* MIS BOLETOS */}
