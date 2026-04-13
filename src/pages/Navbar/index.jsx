aa68e7f0841f6c6a30f040f278fa9c81c92808c3
urbinakevin197-cmyk <urbinakevin197@gmail.com>
2026-04-13 14:58:13 -0600

    LETICIA

--- src/pages/Navbar/index.jsx
+++ src/pages/Navbar/index.jsx
@@ -49,7 +49,7 @@ useEffect(() => {
         {/* BIENVENIDA */}
         <div className="welcome">
           <span>¡Bienvenido de vuelta!</span>
-          <strong>Fernanda Rincón</strong>
+          <strong>Leticia Zamora</strong>
         </div>
 
         {/* MIS BOLETOS */}
