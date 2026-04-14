18c4ab9af6963b30e2f5a3c8811b3e0bf0d2e41a
urbinakevin197-cmyk <urbinakevin197@gmail.com>
2026-04-14 17:09:44 -0600

    citla

--- src/pages/Navbar/index.jsx
+++ src/pages/Navbar/index.jsx
@@ -49,7 +49,7 @@ useEffect(() => {
         {/* BIENVENIDA */}
         <div className="welcome">
           <span>¡Bienvenido de vuelta!</span>
-          <strong>Joselyn Franco</strong>
+          <strong>Citlaly Chávez</strong>
         </div>
 
         {/* MIS BOLETOS */}
