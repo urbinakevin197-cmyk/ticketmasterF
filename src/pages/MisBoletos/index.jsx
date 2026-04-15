8914fb653cc05fe5589479fd83596179341a72b3
urbinakevin197-cmyk <urbinakevin197@gmail.com>
2026-04-14 18:54:28 -0600

    citla

--- src/pages/MisBoletos/index.jsx
+++ src/pages/MisBoletos/index.jsx
@@ -640,7 +640,7 @@ useEffect(() => {
                     ¡Bienvenido de vuelta!
                   </span>
                   <span className="sc-8486e8e9-3 iTJOrD" data-cs-mask="true">
-                    Jorge Hernández
+                    Citlaly Chávez
                   </span>
                 </p>
               </div>
