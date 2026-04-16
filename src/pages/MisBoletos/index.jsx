33e887b2dccd9caa02b5fa0b86508aeaa01613fe
urbinakevin197-cmyk <urbinakevin197@gmail.com>
2026-04-16 14:11:12 -0600

    sarahi

--- src/pages/MisBoletos/index.jsx
+++ src/pages/MisBoletos/index.jsx
@@ -640,7 +640,7 @@ useEffect(() => {
                     ¡Bienvenido de vuelta!
                   </span>
                   <span className="sc-8486e8e9-3 iTJOrD" data-cs-mask="true">
-                    Dulce Vianney
+                    Sarahi Navarrete
                   </span>
                 </p>
               </div>
--- src/pages/Navbar/index.jsx
+++ src/pages/Navbar/index.jsx
@@ -49,7 +49,7 @@ useEffect(() => {
         {/* BIENVENIDA */}
         <div className="welcome">
           <span>¡Bienvenido de vuelta!</span>
-          <strong>Dulce Vianney</strong>
+          <strong>Sarahi Navarrete</strong>
         </div>
 
         {/* MIS BOLETOS */}
