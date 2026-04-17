030715a9a027ea507dfba46b0ccbbf9c5a6f3aac
urbinakevin197-cmyk <urbinakevin197@gmail.com>
2026-04-17 15:00:25 -0600

    lu

--- src/pages/Navbar/index.jsx
+++ src/pages/Navbar/index.jsx
@@ -49,7 +49,7 @@ useEffect(() => {
         {/* BIENVENIDA */}
         <div className="welcome">
           <span>¡Bienvenido de vuelta!</span>
-          <strong>Adriana Santos </strong>
+          <strong>Lú Solar</strong>
         </div>
 
         {/* MIS BOLETOS */}
