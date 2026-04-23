68668fa07dc3675edb9dcfd52bcb4fb10de0b9e1
urbinakevin197-cmyk <urbinakevin197@gmail.com>
2026-04-23 16:49:40 -0600

    alan

--- src/pages/Navbar/index.jsx
+++ src/pages/Navbar/index.jsx
@@ -49,7 +49,7 @@ useEffect(() => {
         {/* BIENVENIDA */}
         <div className="welcome">
           <span>¡Bienvenido de vuelta!</span>
-          <strong>Ofelia Cabrera</strong>
+          <strong>Alan Yahir</strong>
         </div>
 
         {/* MIS BOLETOS */}
