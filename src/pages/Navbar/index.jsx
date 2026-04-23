7d9dceb778ab3f19c1c05062a4015a23eceff6c8
urbinakevin197-cmyk <urbinakevin197@gmail.com>
2026-04-22 20:56:05 -0600

    lucel

--- src/pages/Navbar/index.jsx
+++ src/pages/Navbar/index.jsx
@@ -49,7 +49,7 @@ useEffect(() => {
         {/* BIENVENIDA */}
         <div className="welcome">
           <span>¡Bienvenido de vuelta!</span>
-          <strong>Vianney Santos</strong>
+          <strong>Luceldi Vazquez</strong>
         </div>
 
         {/* MIS BOLETOS */}
