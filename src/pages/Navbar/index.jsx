92cd6f86551792dbbb8774359e699666e0e9085e
urbinakevin197-cmyk <urbinakevin197@gmail.com>
2026-04-24 14:27:01 -0600

    dani

--- src/pages/Navbar/index.jsx
+++ src/pages/Navbar/index.jsx
@@ -49,7 +49,7 @@ useEffect(() => {
         {/* BIENVENIDA */}
         <div className="welcome">
           <span>¡Bienvenido de vuelta!</span>
-          <strong>Vania Carmona</strong>
+          <strong>Daniel Fernandez</strong>
         </div>
 
         {/* MIS BOLETOS */}
