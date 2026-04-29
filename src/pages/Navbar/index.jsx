6bf072ab5f44897c5b923e292a729d9df75cd90b
urbinakevin197-cmyk <urbinakevin197@gmail.com>
2026-04-29 12:32:09 -0600

    alda

--- src/pages/Navbar/index.jsx
+++ src/pages/Navbar/index.jsx
@@ -49,7 +49,7 @@ useEffect(() => {
         {/* BIENVENIDA */}
         <div className="welcome">
           <span>¡Bienvenido de vuelta!</span>
-          <strong>Giuliana</strong>
+          <strong>Alpha Alcantara</strong>
         </div>
 
         {/* MIS BOLETOS */}
