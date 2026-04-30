2ea230db6b3b644f4bfa9aa525e8c757564c5e21
urbinakevin197-cmyk <urbinakevin197@gmail.com>
2026-04-30 11:31:51 -0600

    cambio url222

--- src/pages/BoletoDetalle/index.jsx
+++ src/pages/BoletoDetalle/index.jsx
@@ -15,7 +15,7 @@ export default function BoletoDetalle() {
   const [boleto, setBoleto] = useState(null);
 
     useEffect(() => {
-  fetch(`https://mailto-antenna-aka-founded.trycloudflare.com/api/boletos/${id}`, { //hola we
+  fetch(` https://surrounding-fighter-chips-airfare.trycloudflare.com/api/boletos/${id}`, { //hola we
     headers: {
       Authorization: `Bearer ${localStorage.getItem("token")}`
     }
--- src/pages/TransferDetalle/index.jsx
+++ src/pages/TransferDetalle/index.jsx
@@ -18,7 +18,7 @@ export default function TransferDetalle() {
 
 useEffect(() => {
   const obtener = async () => {
-    const res = await fetch("https://mailto-antenna-aka-founded.trycloudflare.com/api/mis-transferencias", {
+    const res = await fetch(" https://surrounding-fighter-chips-airfare.trycloudflare.com/api/mis-transferencias", {
       headers: {
         Authorization: `Bearer ${localStorage.getItem("token")}`
       }
@@ -39,7 +39,7 @@ const aceptarBoleto = async () => {
   setLoading(true);
 
   try {
-    await fetch(`https://mailto-antenna-aka-founded.trycloudflare.com/api/aceptar-boleto/${t.id}`, {
+    await fetch(` https://surrounding-fighter-chips-airfare.trycloudflare.com/api/aceptar-boleto/${t.id}`, {
       method: "POST",
       headers: {
         Authorization: `Bearer ${localStorage.getItem("token")}`
