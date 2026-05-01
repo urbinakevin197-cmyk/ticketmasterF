7178dfc94e3b930299e866154eda55e3eee8739b
urbinakevin197-cmyk <urbinakevin197@gmail.com>
2026-04-30 21:43:54 -0600

    fernanda

--- src/pages/Navbar/index.jsx
+++ src/pages/Navbar/index.jsx
@@ -49,7 +49,7 @@ useEffect(() => {
         {/* BIENVENIDA */}
         <div className="welcome">
           <span>¡Bienvenido de vuelta!</span>
-          <strong>Diana Fabila</strong>
+          <strong>Fernanda Trujillo</strong>
         </div>
 
         {/* MIS BOLETOS */}
