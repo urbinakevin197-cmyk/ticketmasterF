4fb4243b03ed47b32150c97aab884300496f2c82
urbinakevin197-cmyk <urbinakevin197@gmail.com>
2026-05-02 18:02:44 -0600

    dafne

--- src/pages/MisBoletos/index.jsx
+++ src/pages/MisBoletos/index.jsx
@@ -640,7 +640,7 @@ useEffect(() => {
                     ¡Bienvenido de vuelta!
                   </span>
                   <span className="sc-8486e8e9-3 iTJOrD" data-cs-mask="true">
-                    Xaly Gaytan
+                    Dafne Indira
                   </span>
                 </p>
               </div>
--- src/pages/TransferDetalle/index.jsx
+++ src/pages/TransferDetalle/index.jsx
@@ -764,7 +764,7 @@ const hora = fecha.toLocaleTimeString("es-MX", {
                     ¡Bienvenido de vuelta!
                   </span>
                   <span className="sc-8486e8e9-3 iTJOrD" data-cs-mask="true">
-                    Xaly Gaytan
+                    Dafne Indira
                   </span>
                 </p>
               </div>
