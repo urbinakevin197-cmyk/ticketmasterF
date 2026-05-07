2567d6f51e428df8225c1dd078c409c75c50c72c
urbinakevin197-cmyk <urbinakevin197@gmail.com>
2026-05-06 21:24:10 -0600

    juan

--- src/pages/MisBoletos/index.jsx
+++ src/pages/MisBoletos/index.jsx
@@ -640,7 +640,7 @@ useEffect(() => {
                     ¡Bienvenido de vuelta!
                   </span>
                   <span className="sc-8486e8e9-3 iTJOrD" data-cs-mask="true">
-                    Esli González
+                    Juan García
                   </span>
                 </p>
               </div>
--- src/pages/Navbar/index.jsx
+++ src/pages/Navbar/index.jsx
@@ -49,7 +49,7 @@ useEffect(() => {
         {/* BIENVENIDA */}
         <div className="welcome">
           <span>¡Bienvenido de vuelta!</span>
-          <strong>Esli González</strong>
+          <strong>Juan García</strong>
         </div>
 
         {/* MIS BOLETOS */}
