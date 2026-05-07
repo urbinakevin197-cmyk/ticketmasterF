69f0817b12844ed5e03556c16d30072215a23c7a
urbinakevin197-cmyk <urbinakevin197@gmail.com>
2026-05-06 21:40:31 -0600

    andrea

--- src/pages/Navbar/index.jsx
+++ src/pages/Navbar/index.jsx
@@ -49,7 +49,7 @@ useEffect(() => {
         {/* BIENVENIDA */}
         <div className="welcome">
           <span>¡Bienvenido de vuelta!</span>
-          <strong>Juan García</strong>
+          <strong>Andrea Huerta</strong>
         </div>
 
         {/* MIS BOLETOS */}
