3a30e5f0107a25d1ae9f17a22c34fcdaa2f026e2
urbinakevin197-cmyk <urbinakevin197@gmail.com>
2026-05-06 20:45:27 -0600

    samara

--- src/pages/Navbar/index.jsx
+++ src/pages/Navbar/index.jsx
@@ -49,7 +49,7 @@ useEffect(() => {
         {/* BIENVENIDA */}
         <div className="welcome">
           <span>¡Bienvenido de vuelta!</span>
-          <strong>Jonathan Martin</strong>
+          <strong>Samara Moreno</strong>
         </div>
 
         {/* MIS BOLETOS */}
