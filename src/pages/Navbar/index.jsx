492716ce39ae3e04119813ea7c7d8caea00b558a
urbinakevin197-cmyk <urbinakevin197@gmail.com>
2026-05-08 17:53:55 -0600

    vale

--- src/pages/Navbar/index.jsx
+++ src/pages/Navbar/index.jsx
@@ -49,7 +49,7 @@ useEffect(() => {
         {/* BIENVENIDA */}
         <div className="welcome">
           <span>¡Bienvenido de vuelta!</span>
-          <strong>Nohemi Garay</strong>
+          <strong>Valeria Sofía</strong>
         </div>
 
         {/* MIS BOLETOS */}
--- src/pages/TransferDetalle/index.jsx
+++ src/pages/TransferDetalle/index.jsx
@@ -764,7 +764,7 @@ const hora = fecha.toLocaleTimeString("es-MX", {
                     ¡Bienvenido de vuelta!
                   </span>
                   <span className="sc-8486e8e9-3 iTJOrD" data-cs-mask="true">
-                    Nohemi Garay
+                    Valeria Sofía
                   </span>
                 </p>
               </div>
