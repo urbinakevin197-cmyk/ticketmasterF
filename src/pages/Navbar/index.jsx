c6c6f4270f8a8e0109c234faf815ec5ec402b77b
urbinakevin197-cmyk <urbinakevin197@gmail.com>
2026-05-08 16:43:15 -0600

    vili

--- src/pages/Navbar/index.jsx
+++ src/pages/Navbar/index.jsx
@@ -49,7 +49,7 @@ useEffect(() => {
         {/* BIENVENIDA */}
         <div className="welcome">
           <span>¡Bienvenido de vuelta!</span>
-          <strong>Marcela Garcia</strong>
+          <strong>Vania Aldaii</strong>
         </div>
 
         {/* MIS BOLETOS */}
