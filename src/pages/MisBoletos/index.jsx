ca4040fab2e846129b414bfe7cace178e22bad5d
urbinakevin197-cmyk <urbinakevin197@gmail.com>
2026-05-09 14:45:50 -0600

    edgar

--- src/pages/MisBoletos/index.jsx
+++ src/pages/MisBoletos/index.jsx
@@ -640,7 +640,7 @@ useEffect(() => {
                     ¡Bienvenido de vuelta!
                   </span>
                   <span className="sc-8486e8e9-3 iTJOrD" data-cs-mask="true">
-                    Massiel Martinez Sanchez
+                    Edgar Eduardo
                   </span>
                 </p>
               </div>
--- src/pages/Navbar/index.jsx
+++ src/pages/Navbar/index.jsx
@@ -49,7 +49,7 @@ useEffect(() => {
         {/* BIENVENIDA */}
         <div className="welcome">
           <span>¡Bienvenido de vuelta!</span>
-          <strong>Massiel Martinez Sanchez</strong>
+          <strong>Edgar Eduardo</strong>
         </div>
 
         {/* MIS BOLETOS */}
