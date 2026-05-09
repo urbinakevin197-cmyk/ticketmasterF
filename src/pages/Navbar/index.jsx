2d4bad3570ccc9c0b685a3afcbc9ad5690f0528f
urbinakevin197-cmyk <urbinakevin197@gmail.com>
2026-05-09 13:17:11 -0600

    bet

--- src/pages/Navbar/index.jsx
+++ src/pages/Navbar/index.jsx
@@ -49,7 +49,7 @@ useEffect(() => {
         {/* BIENVENIDA */}
         <div className="welcome">
           <span>¡Bienvenido de vuelta!</span>
-          <strong>Yolanda Esthibaliz</strong>
+          <strong>Betzaida Cruz Benito</strong>
         </div>
 
         {/* MIS BOLETOS */}
