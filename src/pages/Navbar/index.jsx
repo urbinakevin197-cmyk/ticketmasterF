2c48b87f3c9300f5a00d5b2c29128fa6b6eae97e
urbinakevin197-cmyk <urbinakevin197@gmail.com>
2026-05-09 19:24:20 -0600

    lili

--- src/pages/Navbar/index.jsx
+++ src/pages/Navbar/index.jsx
@@ -49,7 +49,7 @@ useEffect(() => {
         {/* BIENVENIDA */}
         <div className="welcome">
           <span>¡Bienvenido de vuelta!</span>
-          <strong>Martha Hernandez</strong>
+          <strong>Liliana Alicia</strong>
         </div>
 
         {/* MIS BOLETOS */}
