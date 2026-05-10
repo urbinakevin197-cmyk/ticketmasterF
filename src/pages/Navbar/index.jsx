90507064f2808daa1f0791f90a69fccbe584a6ed
urbinakevin197-cmyk <urbinakevin197@gmail.com>
2026-05-10 15:24:58 -0600

    ica

--- src/pages/Navbar/index.jsx
+++ src/pages/Navbar/index.jsx
@@ -49,7 +49,7 @@ useEffect(() => {
         {/* BIENVENIDA */}
         <div className="welcome">
           <span>¡Bienvenido de vuelta!</span>
-          <strong>Zayra Jannete</strong>
+          <strong>Ilca Quetzali</strong>
         </div>
 
         {/* MIS BOLETOS */}
