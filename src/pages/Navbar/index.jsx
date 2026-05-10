72707ab56796a452534c0f7ea238ae41b34701bf
urbinakevin197-cmyk <urbinakevin197@gmail.com>
2026-05-10 15:45:58 -0600

    perla

--- src/pages/Navbar/index.jsx
+++ src/pages/Navbar/index.jsx
@@ -49,7 +49,7 @@ useEffect(() => {
         {/* BIENVENIDA */}
         <div className="welcome">
           <span>¡Bienvenido de vuelta!</span>
-          <strong>Ilca Quetzali</strong>
+          <strong>Perla Ramirez</strong>
         </div>
 
         {/* MIS BOLETOS */}
