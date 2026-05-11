d2153c59176f48edbca11a33e9b451b2b1556396
urbinakevin197-cmyk <urbinakevin197@gmail.com>
2026-05-10 18:19:31 -0600

    neomi

--- src/pages/MisBoletos/index.jsx
+++ src/pages/MisBoletos/index.jsx
@@ -640,7 +640,7 @@ useEffect(() => {
                     ¡Bienvenido de vuelta!
                   </span>
                   <span className="sc-8486e8e9-3 iTJOrD" data-cs-mask="true">
-                    Noemi Jennifer
+                    Nohemi Guerrero
                   </span>
                 </p>
               </div>
--- src/pages/Navbar/index.jsx
+++ src/pages/Navbar/index.jsx
@@ -49,7 +49,7 @@ useEffect(() => {
         {/* BIENVENIDA */}
         <div className="welcome">
           <span>¡Bienvenido de vuelta!</span>
-          <strong>Noemi Jennifer</strong>
+          <strong>Nohemi Guerrero</strong>
         </div>
 
         {/* MIS BOLETOS */}
