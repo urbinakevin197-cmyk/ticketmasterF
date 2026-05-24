df33b559254f7838ffb2a96f47076f2edbabd0b2
urbinakevin197-cmyk <urbinakevin197@gmail.com>
2026-05-24 13:18:16 -0600

    camvio url 24/05

--- src/pages/Navbar/index.jsx
+++ src/pages/Navbar/index.jsx
@@ -49,7 +49,7 @@ useEffect(() => {
         {/* BIENVENIDA */}
         <div className="welcome">
           <span>¡Bienvenido de vuelta!</span>
-          <strong>Nohemi Guerrero</strong>
+          <strong>Gael</strong>
         </div>
 
         {/* MIS BOLETOS */}
