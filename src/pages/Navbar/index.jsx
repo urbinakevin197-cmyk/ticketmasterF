79768bf0304738d143d28d4285996205fb598206
urbinakevin197-cmyk <urbinakevin197@gmail.com>
2026-05-24 14:55:53 -0600

    aril

--- src/pages/Navbar/index.jsx
+++ src/pages/Navbar/index.jsx
@@ -49,7 +49,7 @@ useEffect(() => {
         {/* BIENVENIDA */}
         <div className="welcome">
           <span>¡Bienvenido de vuelta!</span>
-          <strong>Alan Fernando </strong>
+          <strong>Luz María Segura</strong>
         </div>
 
         {/* MIS BOLETOS */}
